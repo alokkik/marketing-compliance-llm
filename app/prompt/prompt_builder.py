302fa0ed41f4560bdab579d86a462929f7967b79
alokkik <imallok@yahoo.in>
2024-02-05 00:50:37 +0530

    chore: ready to share

--- app/prompt/prompt_builder.py
+++
@@ -1,9 +0,0 @@
-from llama_index.prompts import PromptTemplate
-
-class PromptBuilder:
-    def __init__(self, template):
-        self.template = template
-
-    def build_prompt(self, **kwargs):
-        qa_template = PromptTemplate(self.template)
-        return qa_template.format(**kwargs)
